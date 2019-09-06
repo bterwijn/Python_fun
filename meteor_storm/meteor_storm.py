f6006e87823afdc9b1f01ee43e5c661ffd4d7459
Bas Terwijn <bterwijn@gmail.com>
2019-09-06 15:59:15 +0200

    update

--- meteor_storm/meteor_storm.py
+++ meteor_storm/meteor_storm.py
@@ -88,6 +88,7 @@ class Meteor:
     self.speed=random.uniform(min_speed,max_speed)
     r=(self.speed-min_speed)/(max_speed-min_speed)
     self.posrad=PosRad(x,y,6+10-r*10)
+    self.color="green"
 
   def time_step(self):
     self.posrad.x-=self.speed
@@ -97,7 +98,7 @@ class Meteor:
   def draw(self,canvas):
     canvas.create_oval(self.posrad.x-self.posrad.rad, self.posrad.y-self.posrad.rad, \
                        self.posrad.x+self.posrad.rad, self.posrad.y+self.posrad.rad, \
-                       fill="red", outline="white")
+                       fill=self.color, outline="white")
 
 class Ship:
 
@@ -108,19 +109,19 @@ class Ship:
     self.vy=0
 
   def time_step(self):
-    if keyboard.is_down("Left"):
+    if keyboard.is_down("Left") or keyboard.is_down("z"):
       self.vx-=self.speed
       canvas.create_line(self.posrad.x+self.posrad.rad   , self.posrad.y,
                          self.posrad.x+self.posrad.rad+20, self.posrad.y, fill="red", width=10)
-    if keyboard.is_down("Right"):
+    if keyboard.is_down("Right") or keyboard.is_down("x"):
       self.vx+=self.speed
       canvas.create_line(self.posrad.x-self.posrad.rad   , self.posrad.y,
                          self.posrad.x-self.posrad.rad-20, self.posrad.y, fill="red", width=10)
-    if keyboard.is_down("Up"):
+    if keyboard.is_down("Up") or keyboard.is_down("apostrophe"):
       self.vy-=self.speed
       canvas.create_line(self.posrad.x , self.posrad.y+self.posrad.rad,
                          self.posrad.x , self.posrad.y+self.posrad.rad+20, fill="red", width=10)
-    if keyboard.is_down("Down"):
+    if keyboard.is_down("Down") or keyboard.is_down("slash"):
       self.vy+=self.speed
       canvas.create_line(self.posrad.x , self.posrad.y-self.posrad.rad,
                          self.posrad.x , self.posrad.y-self.posrad.rad-20, fill="red", width=10)
@@ -145,7 +146,7 @@ class Ship:
   def draw(self,canvas):
     canvas.create_oval(self.posrad.x-self.posrad.rad, self.posrad.y-self.posrad.rad, \
                        self.posrad.x+self.posrad.rad, self.posrad.y+self.posrad.rad, \
-                       fill="black", outline="white")
+                       fill=None, outline="white", width=5)
 
   def collision_detect(self,meteors):
     for i in meteors:
@@ -177,10 +178,10 @@ def time_step():
 
 def key_down(e):
     keyboard.key_down(e.keysym)
-    #print('down',e.keysym)
+    #print('down:',e.keysym)
 def key_up(e):
     keyboard.key_up(e.keysym)
-    #print('up',e.keysym)
+    #print('up:',e.keysym)
     
 root.bind("<KeyPress>", key_down)
 root.bind("<KeyRelease>", key_up)
