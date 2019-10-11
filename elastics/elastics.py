19d2bc7d38c2222a095184f4814501d1d1b4eb21
Bas Terwijn <bterwijn@gmail.com>
2019-10-11 20:40:58 +0200

    move global variables to Globals class

--- elastics/elastics.py
+++ elastics/elastics.py
@@ -7,17 +7,18 @@ import random
 
 print("Drag balls with mouse:")
 
-root = tkinter.Tk()
-root.title("Elastics")  
-canvas_width=1000
-canvas_height=800
-canvas = tkinter.Canvas(root, width=canvas_width, height=canvas_height)
-canvas.config(bg="black")
-canvas.pack()
-canvas.focus_set()
-time=0
-balls=[]
-mouse_holds_ball=None
+class Globals:
+  root = tkinter.Tk()
+  root.title("Elastics")  
+  canvas_width=1000
+  canvas_height=800
+  canvas = tkinter.Canvas(root, width=canvas_width, height=canvas_height)
+  canvas.config(bg="black")
+  canvas.pack()
+  canvas.focus_set()
+  time=0
+  balls=[]
+  mouse_holds_ball=None
 
 class Ball:
   size=20
@@ -36,7 +37,7 @@ class Ball:
 def closest_ball(mx,my):
   closest=None
   min_distance=None
-  for i in balls:
+  for i in Globals.balls:
     sd=i.sqaure_distance(mx,my)
     if min_distance is None or sd<min_distance:
       min_distance=sd
@@ -47,12 +48,12 @@ def move_balls():
   gravity=3
   force=0.1
   air_resistance=0.9
-  for i in range(1,len(balls)-1):
-    b=balls[i]
-    fx=balls[i-1].x-b.x + balls[i+1].x-b.x
-    fy=balls[i-1].y-b.y + balls[i+1].y-b.y
+  for i in range(1,len(Globals.balls)-1):
+    b=Globals.balls[i]
+    fx=Globals.balls[i-1].x-b.x + Globals.balls[i+1].x-b.x
+    fy=Globals.balls[i-1].y-b.y + Globals.balls[i+1].y-b.y
     fy+=gravity
-    if not b is mouse_holds_ball:
+    if not b is Globals.mouse_holds_ball:
       b.vx+= fx*force
       b.vy+= fy*force
       b.x+= b.vx
@@ -61,48 +62,49 @@ def move_balls():
       b.vy*=air_resistance
 
 def draw_balls():
-  for i in range(len(balls)):
-    canvas.create_oval(balls[i].x-Ball.size, balls[i].y-Ball.size, \
-                       balls[i].x+Ball.size, balls[i].y+Ball.size, \
-                       fill=None, outline="white", width=6)
-    if i+1<len(balls):
-      canvas.create_line(balls[i  ].x, balls[i  ].y, \
-                         balls[i+1].x, balls[i+1].y, \
-                         fill="white", width=4)
+  for i in range(len(Globals.balls)):
+    Globals.canvas.create_oval(Globals.balls[i].x-Ball.size, Globals.balls[i].y-Ball.size, \
+                               Globals.balls[i].x+Ball.size, Globals.balls[i].y+Ball.size, \
+                               fill=None, outline="white", width=6)
+    if i+1<len(Globals.balls):
+      Globals.canvas.create_line(Globals.balls[i  ].x, Globals.balls[i  ].y, \
+                                 Globals.balls[i+1].x, Globals.balls[i+1].y, \
+                                 fill="white", width=4)
 
 def mouse_button(event):
-  global mouse_holds_ball
-  mouse_holds_ball=closest_ball(event.x,event.y)
-  mouse_holds_ball.x=event.x
-  mouse_holds_ball.y=event.y
+  Globals.mouse_holds_ball=closest_ball(event.x,event.y)
+  Globals.mouse_holds_ball.x=event.x
+  Globals.mouse_holds_ball.y=event.y
   
 def mouse_release(event):
-  global mouse_holds_ball
   #print("Mouse release:",event)
-  mouse_holds_ball=None
+  Globals.mouse_holds_ball=None
   
 def mouse_motion(event):
-  global mouse_holds_ball
   #print("Mouse motion:",event)
-  if not mouse_holds_ball is None:
-    mouse_holds_ball.x=event.x
-    mouse_holds_ball.y=event.y
+  if not Globals.mouse_holds_ball is None:
+    Globals.mouse_holds_ball.x=event.x
+    Globals.mouse_holds_ball.y=event.y
   
-nr_balls=20
-for i in range(nr_balls):
-  balls.append(Ball(i*canvas_width/(nr_balls-1), canvas_height*1/3))
-
 def time_step():
-    global time
-    canvas.delete("all") # remove all previous drawings
-    time+=1
+    Globals.canvas.delete("all") # remove all previous drawings
+    Globals.time+=1
     move_balls()
     draw_balls()
-    root.after(10, time_step)
+    Globals.root.after(10, time_step)
 
-root.update()
-root.bind('<Button>',mouse_button)
-root.bind('<ButtonRelease>',mouse_release)
-root.bind('<Motion>',mouse_motion)
-root.after(100, time_step)
-root.mainloop()
+def main():
+  Globals.root.update()
+  Globals.root.bind('<Button>',mouse_button)
+  Globals.root.bind('<ButtonRelease>',mouse_release)
+  Globals.root.bind('<Motion>',mouse_motion)
+
+  nr_balls=20
+  for i in range(nr_balls):
+    Globals.balls.append(Ball(i*Globals.canvas_width/(nr_balls-1), Globals.canvas_height*1/3))
+    
+  Globals.root.after(100, time_step)
+  Globals.root.mainloop()
+
+if __name__ == "__main__":
+    main()
